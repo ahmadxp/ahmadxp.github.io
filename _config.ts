2e1690c0377514e9323e40f7e3fcd36fe23e22ba
afokay <ahmadnitro@proton.me>
2023-09-07 07:12:20 +0700

    update

--- _config.ts
+++ _config.ts
@@ -1,11 +1,10 @@
 import lume from "lume/mod.ts";
 import code_highlight from "lume/plugins/code_highlight.ts";
 import date from "lume/plugins/date.ts";
-import metas from "lume/plugins/metas.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
-import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
+import feed from "lume/plugins/feed.ts";
 
 const site = lume({
     components: {
@@ -19,10 +18,20 @@ site.copy("static", ".");
 
 site.use(code_highlight());
 site.use(date());
-site.use(metas());
 site.use(sitemap());
 site.use(tailwindcss());
-site.use(tailwindcss());
 site.use(postcss());
+site.use(feed({
+    output: ["/feed.json", "/feed.xml"],
+    query: "type=posts",
+    info: {
+        title: "=site.title",
+        description: "=site.description",
+    },
+    items: {
+        title: "=title",
+        content: "$.post-body",
+    },
+}));
 
 export default site;
---
+++ posts/hello-world.md
@@ -0,0 +1,5 @@
+---
+title: Hello World
+description: Description Hello World
+url: "../hello-world/"
+---
