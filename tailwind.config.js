cb35d82cd2b791ef22ba812294718c6be58c78aa
afokay <ahmadnitro@proton.me>
2023-09-09 06:49:00 +0700

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     fontFamily: {
       'sans': ['Quicksand', 'sans-serif'],
     },
     colors: {
       'black': '#030712',
+      'black2': '#10141e',
       'grey': '#4A4A48',
-      'navy': '#61677A',
+      'navy': '#18122B',
       'white': '#EBEBE3',
     },
   },
