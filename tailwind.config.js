fe4fc8f84cb016632b47b0d13b0b2e8de3437307
afokay <ahmadnitro@proton.me>
2023-09-08 17:21:44 +0700

    update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
     fontFamily: {
       'sans': ['Quicksand', 'sans-serif'],
